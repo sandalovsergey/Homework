b7a09121e3ad3fdc28cc5b0e5027ba19bc9f13c2
artemaliev <artem.aliev@gmail,com>
2016-09-30 17:31:33 +0300

    add tests

--- Matrix/src/com/company/Matrix.java
+++ Matrix/src/com/company/Matrix.java
@@ -4,5 +4,5 @@ package com.company;
 public interface Matrix
 {
   Matrix mul (Matrix other);
-
+  boolean equals(Object o);
 }
--- Matrix/tests/com/company/MatrixTest.java
+++ Matrix/tests/com/company/MatrixTest.java
@@ -11,9 +11,32 @@ public class MatrixTest
 {
 
   @Test
-  public void mullTest(){
-
-    assertEquals(2,2);
+  public void mulSDTest(){
+    Matrix a = new SMat("file1");
+    Matrix b =  new DMat("file2");
+    Matrix res =  new DMat("result2");
+    assertEquals(res, a.mul(b));
   }
 
+  @Test
+  public void mulDDTest(){
+    Matrix a = new DMat("file1");
+    Matrix b =  new DMat("file2");
+    Matrix res =  new DMat("result2");
+    assertEquals(res, a.mul(b));
+  }
+  @Test
+  public void mulDSTest(){
+    Matrix a = new DMat("file1");
+    Matrix b =  new SMat("file2");
+    Matrix res =  new DMat("result2");
+    assertEquals(res, a.mul(b));
+  }
+  @Test
+  public void mulSSTest(){
+    Matrix a = new SMat("file1");
+    Matrix b =  new SMat("file2");
+    Matrix res =  new DMat("result2");
+    assertEquals(res, a.mul(b));
+  }
 }
